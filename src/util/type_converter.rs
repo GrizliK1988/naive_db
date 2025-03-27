7897ef1c181cd5b9c5a1c9ad86c3e6e97fa06fe1
Dmitrii Grachikov <dgrachikov@gmail.com>
2025-03-27 20:37:57 +0000

    Remove unneeded code

--- src/util/type_converter.rs
+++ src/util/type_converter.rs
@@ -2,16 +2,6 @@ pub fn int_to_bytes(value: &i32) -> [u8; 4] {
     value.to_be_bytes()
 }
 
-pub fn bytes_to_int(bytes: &[u8; 4]) -> i32 {
-    i32::from_be_bytes(*bytes)
-}
-
 pub fn string_to_bytes(value: &str) -> &[u8] {
     value.as_bytes()
 }
-
-pub fn bytes_to_string(bytes: &[u8]) -> String {
-    unsafe {
-        String::from_utf8_unchecked(bytes.to_vec())
-    }
-} 
